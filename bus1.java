8d02c7fd362c5350768c8cbcf5b994d8143adef4
beautyalifyaa <beautyalifyamaritza.m@gmail.com>
2024-01-18 08:59:24 +0700

    Berhasil

---
+++ bus1.java
@@ -0,0 +1,10 @@
+package bus;
+
+public class bus1 {
+    public int penumpang, maxPenumpang;
+    public void cetak(){
+        System.out.println("Penumpang sekarang = " + penumpang);
+        System.out.println("Penumpang seharusnya adalah = " + maxPenumpang);   
+    }
+
+}
